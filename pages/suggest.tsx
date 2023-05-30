0cb68ed49200a8f4b616257238cf3cf7619fa735
eunzee <eunjiamy@naver.com>
2023-05-30 22:07:43 +0900

    [suggest] api set & [verbose] Marker

--- pages/suggest.tsx
+++ pages/suggest.tsx
@@ -1,22 +1,624 @@
 import "../styles/suggest.module.css";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { Button } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import { SimpleGrid, Heading, Text } from "@chakra-ui/react";
 
 export default function Suggestion() {
   const router = useRouter();
 
-  // 뭔가(ex: 버튼)를 눌렀을 때 라우터 실행
-  // query에 데이터를 담아서 보낸다. 어떤 데이터를 담을지는 추후논의
-  // router.push({pathname:"/verbose", query}); 이렇게 하면됨
-  // 페이지 실험을 위해 예시버튼 넣어놓겠음!
+  const query = {
+    length: 3,
+    waypointsData: [
+      {
+        name: "홍대입구역[2호선]",
+        lat: "37.55709320",
+        lon: "126.92385960",
+      },
+      {
+        name: "혜화역[4호선]",
+        lat: "37.58236944",
+        lon: "127.00185178",
+      },
+      {
+        name: "서울역",
+        lat: "37.55470543",
+        lon: "126.97068873",
+      },
+    ],
+  };
 
-  const query = {};
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      appKey: "UN27oUnc8Ma0gwH3grXRaat6I163mp0W1Osa5VUy",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      startX: query.waypointsData[0].lon,
+      startY: query.waypointsData[0].lat,
+      endX: query.waypointsData[1].lon,
+      endY: query.waypointsData[1].lat,
+      lang: 0,
+      format: "json",
+      count: 10,
+    }),
+  };
+
+  const [data1, setData1] = useState(null);
+  const [isLoading1, setIsloading1] = useState(true);
+
+  const [data2, setData2] = useState(null);
+  const [isLoading2, setIsloading2] = useState(true);
+
+  const [data3, setData3] = useState(null);
+  const [isLoading3, setIsloading3] = useState(true);
+
+  const [data4, setData4] = useState(null);
+  const [isLoading4, setIsloading4] = useState(true);
+
+  const [data5, setData5] = useState(null);
+  const [isLoading5, setIsloading5] = useState(true);
+
+  const [data6, setData6] = useState(null);
+  const [isLoading6, setIsloading6] = useState(true);
+
+  var via = query.length; //경유지 개수(앞에서 받아오기)
+  via -= 2;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://apis.openapi.sk.com/transit/routes",
+          options
+        );
+        const json = await response.json();
+        setData1(json);
+        setIsloading1(false);
+      } catch (error) {
+        console.error(error);
+        setIsloading1(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const options = {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          appKey: "UN27oUnc8Ma0gwH3grXRaat6I163mp0W1Osa5VUy",
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          startX: query.waypointsData[1].lon,
+          startY: query.waypointsData[1].lat,
+          endX: query.waypointsData[2].lon,
+          endY: query.waypointsData[2].lat,
+          lang: 0,
+          format: "json",
+          count: 10,
+        }),
+      };
+      try {
+        const response = await fetch(
+          "https://apis.openapi.sk.com/transit/routes",
+          options
+        );
+        const json = await response.json();
+        setData2(json);
+        setIsloading2(false);
+      } catch (error) {
+        console.error(error);
+        setIsloading2(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (via - 1 > 0) {
+        const options = {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+            appKey: "UN27oUnc8Ma0gwH3grXRaat6I163mp0W1Osa5VUy",
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            startX: query.waypointsData[2].lon,
+            startY: query.waypointsData[2].lat,
+            endX: query.waypointsData[3].lon,
+            endY: query.waypointsData[3].lat,
+            lang: 0,
+            format: "json",
+            count: 10,
+          }),
+        };
+        try {
+          const response = await fetch(
+            "https://apis.openapi.sk.com/transit/routes",
+            options
+          );
+          const json = await response.json();
+          setData3(json);
+          setIsloading3(false);
+        } catch (error) {
+          console.error(error);
+          setIsloading3(false);
+        }
+      } else {
+        setIsloading3(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (via - 2 > 0) {
+        const options = {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+            appKey: "UN27oUnc8Ma0gwH3grXRaat6I163mp0W1Osa5VUy",
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            startX: query.waypointsData[3].lon,
+            startY: query.waypointsData[3].lat,
+            endX: query.waypointsData[4].lon,
+            endY: query.waypointsData[4].lat,
+            lang: 0,
+            format: "json",
+            count: 10,
+          }),
+        };
+        try {
+          const response = await fetch(
+            "https://apis.openapi.sk.com/transit/routes",
+            options
+          );
+          const json = await response.json();
+          setData4(json);
+          setIsloading4(false);
+        } catch (error) {
+          console.error(error);
+          setIsloading4(false);
+        }
+      } else {
+        setIsloading4(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (via - 3 > 0) {
+        const options = {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+            appKey: "UN27oUnc8Ma0gwH3grXRaat6I163mp0W1Osa5VUy",
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            startX: query.waypointsData[4].lon,
+            startY: query.waypointsData[4].lat,
+            endX: query.waypointsData[5].lon,
+            endY: query.waypointsData[5].lat,
+            lang: 0,
+            format: "json",
+            count: 10,
+          }),
+        };
+        try {
+          const response = await fetch(
+            "https://apis.openapi.sk.com/transit/routes",
+            options
+          );
+          const json = await response.json();
+          setData5(json);
+          setIsloading5(false);
+        } catch (error) {
+          console.error(error);
+          setIsloading5(false);
+        }
+      } else {
+        setIsloading5(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (via - 4 > 0) {
+        const options = {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+            appKey: "UN27oUnc8Ma0gwH3grXRaat6I163mp0W1Osa5VUy",
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            startX: query.waypointsData[5].lon,
+            startY: query.waypointsData[5].lat,
+            endX: query.waypointsData[6].lon,
+            endY: query.waypointsData[6].lat,
+            lang: 0,
+            format: "json",
+            count: 10,
+          }),
+        };
+        try {
+          const response = await fetch(
+            "https://apis.openapi.sk.com/transit/routes",
+            options
+          );
+          const json = await response.json();
+          setData6(json);
+          setIsloading6(false);
+        } catch (error) {
+          console.error(error);
+          setIsloading6(false);
+        }
+      } else {
+        setIsloading6(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (
+    isLoading1 ||
+    isLoading2 ||
+    isLoading3 ||
+    isLoading4 ||
+    isLoading5 ||
+    isLoading6
+  ) {
+    return <div> Loading... </div>;
+  }
+
+  console.log(data1.metaData.plan.itineraries);
+  console.log(data2.metaData.plan.itineraries);
+
+  const dataList = [];
+  dataList.push(data2);
+  if (via - 1 > 0) {
+    dataList.push(data3);
+  }
+  if (via - 2 > 0) {
+    dataList.push(data4);
+  }
+  if (via - 3 > 0) {
+    dataList.push(data5);
+  }
+  if (via - 4 > 0) {
+    dataList.push(data6);
+  }
+
+  const itinerary = data1.metaData.plan.itineraries;
+  const method1 = new Array(10); //time1
+  const method2 = new Array(10); //time2
+  const method3 = new Array(10); //time3
+  const method4 = new Array(10); //transfer1
+  const method5 = new Array(10); //fare1
+
+  //time sort
+  itinerary.sort(function (a, b) {
+    return a.totalTime - b.totalTime;
+  });
+  method1[0] = itinerary[0];
+  method2[0] = itinerary[1];
+  method3[0] = itinerary[2];
+  method1[6] = itinerary[0].totalWalkTime;
+  method2[6] = itinerary[1].totalWalkTime;
+  method3[6] = itinerary[2].totalWalkTime;
+  method1[7] = itinerary[0].transferCount;
+  method2[7] = itinerary[1].transferCount;
+  method3[7] = itinerary[2].transferCount;
+  method1[8] = itinerary[0].fare.regular.totalFare;
+  method2[8] = itinerary[1].fare.regular.totalFare;
+  method3[8] = itinerary[2].fare.regular.totalFare;
+  method1[9] = itinerary[0].totalTime;
+  method2[9] = itinerary[1].totalTime;
+  method3[9] = itinerary[2].totalTime;
+  //transfer sort
+  itinerary.sort(function (a, b) {
+    if (a.transferCount == b.transferCount) return a.totalTime - b.totalTime;
+    return a.transferCount - b.transferCount;
+  });
+  method4[0] = itinerary[0];
+  method4[6] = itinerary[0].totalWalkTime;
+  method4[7] = itinerary[0].transferCount;
+  method4[8] = itinerary[0].fare.regular.totalFare;
+  method4[9] = itinerary[0].totalTime;
+  //fare sort
+  itinerary.sort(function (a, b) {
+    if (a.fare.regular.totalFare == b.fare.regular.totalFare)
+      return a.totalTime - b.totalTime;
+    return a.fare.regular.totalFare - b.fare.regular.totalFare;
+  });
+  method5[0] = itinerary[0];
+  method5[6] = itinerary[0].totalWalkTime;
+  method5[7] = itinerary[0].transferCount;
+  method5[8] = itinerary[0].fare.regular.totalFare;
+  method5[9] = itinerary[0].totalTime;
+
+  for (var i = 0; i < via; i++) {
+    var second_flag = 1; //time_2 결정할 때 method2가 날아가는 경우
+    //실제로는 이 부분에서 api 활용해서 json 파일 받아오기
+
+    const itinerary2 = dataList[i].metaData.plan.itineraries;
+
+    itinerary2.sort(function (a, b) {
+      return a.totalTime - b.totalTime;
+    });
+
+    method1[i + 1] = itinerary2[0]; //time_1
+    method1[6] += itinerary2[0].totalWalkTime;
+    method1[7] += itinerary2[0].transferCount;
+    method1[8] += itinerary2[0].fare.regular.totalFare;
+    method1[9] += itinerary2[0].totalTime;
+
+    if (
+      method2[9] + itinerary2[0].totalTime >
+      method1[9] + itinerary2[1].totalTime
+    ) {
+      //time_2
+      second_flag = 0;
+      var temp = JSON.parse(JSON.stringify(method2));
+      for (var j = 0; j < 10; j++) method2[j] = method1[j];
+      method2[6] += itinerary2[1].totalWalkTime;
+      method2[7] += itinerary2[1].transferCount;
+      method2[8] += itinerary2[1].fare.regular.totalFare;
+      method2[9] += itinerary2[1].totalTime;
+      method2[i + 1] = itinerary2[1];
+    } else {
+      method2[i + 1] = itinerary2[0];
+      method2[6] += itinerary2[0].totalWalkTime;
+      method2[7] += itinerary2[0].transferCount;
+      method2[8] += itinerary2[0].fare.regular.totalFare;
+      method2[9] += itinerary2[0].totalTime;
+    }
+
+    if (second_flag) {
+      //time_3
+      if (
+        method3[9] + itinerary2[0].totalTime >
+        method1[9] + itinerary2[1].totalTime
+      ) {
+        for (var j = 0; j < 10; j++) method3[j] = method1[j];
+        method3[6] += itinerary2[1].totalWalkTime;
+        method3[7] += itinerary2[1].transferCount;
+        method3[8] += itinerary2[1].fare.regular.totalFare;
+        method3[9] += itinerary2[1].totalTime;
+        method3[i + 1] = itinerary2[1];
+      } else {
+        method3[i + 1] = itinerary2[0];
+        method3[6] += itinerary2[0].totalWalkTime;
+        method3[7] += itinerary2[0].transferCount;
+        method3[8] += itinerary2[0].fare.regular.totalFare;
+        method3[9] += itinerary2[0].totalTime;
+      }
+    } else {
+      if (
+        temp[9] + itinerary2[0].totalTime >
+        method1[9] + itinerary2[2].totalTime
+      ) {
+        for (var j = 0; j < 10; j++) method3[j] = method1[j];
+        method3[6] += itinerary2[2].totalWalkTime;
+        method3[7] += itinerary2[2].transferCount;
+        method3[8] += itinerary2[2].fare.regular.totalFare;
+        method3[9] += itinerary2[2].totalTime;
+        method3[i + 1] = itinerary2[2];
+      } else {
+        for (var j = 0; j < 10; j++) method3[j] = temp[j];
+        method3[6] += itinerary2[0].totalWalkTime;
+        method3[7] += itinerary2[0].transferCount;
+        method3[8] += itinerary2[0].fare.regular.totalFare;
+        method3[9] += itinerary2[0].totalTime;
+        method3[i + 1] = itinerary2[0];
+      }
+    }
+
+    itinerary2.sort(function (a, b) {
+      if (a.transferCount == b.transferCount) return a.totalTime - b.totalTime;
+      return a.transferCount - b.transferCount;
+    });
+    method4[i + 1] = itinerary2[0]; //transfer_1
+    method4[6] += itinerary2[0].totalWalkTime;
+    method4[7] += itinerary2[0].transferCount;
+    method4[8] += itinerary2[0].fare.regular.totalFare;
+    method4[9] += itinerary2[0].totalTime;
+
+    itinerary.sort(function (a, b) {
+      if (a.fare.regular.totalFare == b.fare.regular.totalFare)
+        return a.totalTime - b.totalTime;
+      return a.fare.regular.totalFare - b.fare.regular.totalFare;
+    });
+    method5[i + 1] = itinerary2[0]; //fare_1
+    method5[6] += itinerary2[0].totalWalkTime;
+    method5[7] += itinerary2[0].transferCount;
+    method5[8] += itinerary2[0].fare.regular.totalFare;
+    method5[9] += itinerary2[0].totalTime;
+
+    method1[7] += 1;
+    method2[7] += 1;
+    method3[7] += 1;
+    method4[7] += 1;
+    method5[7] += 1;
+  }
+
+  let today = new Date();
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+
+  var method1_hours = parseInt(method1[9] / 3600);
+  method1[9] -= method1_hours * 3600;
+  var method1_minutes = parseInt(method1[9] / 60);
+  var method2_hours = parseInt(method2[9] / 3600);
+  method2[9] -= method2_hours * 3600;
+  var method2_minutes = parseInt(method2[9] / 60);
+  var method3_hours = parseInt(method3[9] / 3600);
+  method3[9] -= method3_hours * 3600;
+  var method3_minutes = parseInt(method3[9] / 60);
+  var method4_hours = parseInt(method4[9] / 3600);
+  method4[9] -= method4_hours * 3600;
+  var method4_minutes = parseInt(method4[9] / 60);
+  var method5_hours = parseInt(method5[9] / 3600);
+  method5[9] -= method5_hours * 3600;
+  var method5_minutes = parseInt(method5[9] / 60);
 
   return (
     <>
-      <Button onClick={() => router.push({ pathname: "/verbose", query })}>
-        자세히보기
-      </Button>
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      >
+        <Card>
+          <CardHeader>
+            <Heading size="md"> 최소시간 경로1</Heading>
+          </CardHeader>
+          <CardBody>
+            <Heading size="md">
+              {" "}
+              {method1_hours}시간 {method1_minutes}분
+            </Heading>
+            <Text>
+              {hours}:{String(minutes).padStart(2, "0")} ~{" "}
+              {hours + method1_hours}:{minutes + method1_minutes}
+            </Text>
+            <Text>도보: {parseInt(method1[6] / 60)}분 </Text>
+            <Text>환승: {method1[7]}회</Text>
+            <Text>카드: {method1[8]}원</Text>
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup onClick={() => router.push({ pathname: "/verbose" })}>
+              <Button variant="ghost" colorScheme="black">
+                경로 상세보기
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> 최소시간 경로2</Heading>
+          </CardHeader>
+          <CardBody>
+            <Heading size="md">
+              {" "}
+              {method2_hours}시간 {method2_minutes}분
+            </Heading>
+            <Text>
+              {hours}:{String(minutes).padStart(2, "0")} ~{" "}
+              {hours + method2_hours}:{minutes + method2_minutes}
+            </Text>
+            <Text>도보: {parseInt(method2[6] / 60)}분 </Text>
+            <Text>환승: {method2[7]}회</Text>
+            <Text>카드: {method2[8]}원</Text>
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup
+              onClick={() => router.push({ pathname: "/verbose", query })}
+            >
+              <Button variant="ghost" colorScheme="black">
+                경로 상세보기
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> 최소시간 경로3</Heading>
+          </CardHeader>
+          <CardBody>
+            <Heading size="md">
+              {" "}
+              {method3_hours}시간 {method3_minutes}분
+            </Heading>
+            <Text>
+              {hours}:{String(minutes).padStart(2, "0")} ~{" "}
+              {hours + method3_hours}:{minutes + method3_minutes}
+            </Text>
+            <Text>도보: {parseInt(method3[6] / 60)}분 </Text>
+            <Text>환승: {method3[7]}회</Text>
+            <Text>카드: {method3[8]}원</Text>
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup
+              onClick={() => router.push({ pathname: "/verbose", query })}
+            >
+              <Button variant="ghost" colorScheme="black">
+                경로 상세보기
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> 최소환승 경로</Heading>
+          </CardHeader>
+          <CardBody>
+            <Heading size="md">
+              {" "}
+              {method4_hours}시간 {method4_minutes}분
+            </Heading>
+            <Text>
+              {hours}:{String(minutes).padStart(2, "0")} ~{" "}
+              {hours + method4_hours}:{minutes + method4_minutes}
+            </Text>
+            <Text>도보: {parseInt(method4[6] / 60)}분 </Text>
+            <Text>환승: {method4[7]}회</Text>
+            <Text>카드: {method4[8]}원</Text>
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup
+              onClick={() => router.push({ pathname: "/verbose", query })}
+            >
+              <Button variant="ghost" colorScheme="black">
+                경로 상세보기
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> 최소요금 경로</Heading>
+          </CardHeader>
+          <CardBody>
+            <Heading size="md">
+              {" "}
+              {method5_hours}시간 {method5_minutes}분
+            </Heading>
+            <Text>
+              {hours}:{String(minutes).padStart(2, "0")} ~{" "}
+              {hours + method5_hours}:{minutes + method5_minutes}
+            </Text>
+            <Text>도보: {parseInt(method5[6] / 60)}분 </Text>
+            <Text>환승: {method5[7]}회</Text>
+            <Text>카드: {method5[8]}원</Text>
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup
+              onClick={() => router.push({ pathname: "/verbose", query })}
+            >
+              <Button variant="ghost" colorScheme="black">
+                경로 상세보기
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
     </>
   );
 }
--- pages/verbose.tsx
+++ pages/verbose.tsx
@@ -1,12 +1,100 @@
-import { Card, Stack, Box, Flex, Text, HStack } from "@chakra-ui/react";
-import Progressbar from "@/components/Progressbar";
+import {
+  Card,
+  Stack,
+  Box,
+  Flex,
+  Text,
+  HStack,
+  Circle,
+  CardHeader,
+  Heading,
+  CardBody,
+  List,
+} from "@chakra-ui/react";
 import "../styles/verbose.module.css";
 import data from "./dummy.json";
 import Script from "next/script";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
+function verboseBox(leg: any) {
+  var icon: "🚍" | "🚊" | "🚆" | "🚶🏻" = "🚶🏻";
+  var color: string = "gray.200";
+  var verbose: string = "";
+  if (leg.mode === "WALK" || leg.mode === "TRANSFER") {
+    icon = "🚶🏻";
+    color = "gray.200";
+    verbose = "도보로 " + leg.distance + "m 이동";
+  } else if (leg.mode === "BUS") {
+    icon = "🚍";
+    color = "#" + leg.routeColor;
+    verbose = leg.route + "로 " + leg.type + "개 정류장 이동";
+  } else if (leg.mode === "SUBWAY") {
+    icon = "🚊";
+    color = "orange.300";
+    verbose = "지하철";
+  } else if (leg.mode === "TRAIN") {
+    icon = "🚆";
+    color = "#" + leg.routeColor;
+    verbose = "기차";
+  }
+
+  return (
+    <>
+      <Flex width="100%" height="150px" flexDirection="row">
+        <Flex width="40px" height="100%" flexDirection="column">
+          <Circle size="40px" bg={color}>
+            <Text fontSize="xl">{icon}</Text>
+          </Circle>
+          <Box
+            width="10px"
+            flex="1"
+            bg={color}
+            alignSelf="center"
+            marginTop="-2"
+            marginBottom="-4"
+          />
+        </Flex>
+        <Card
+          flex="1"
+          marginLeft="4"
+          shadow="lg"
+          onClick={() => {
+            <Script id="onClickCard">{`moveCenter(${leg.start.lat}, ${leg.start.lon})`}</Script>;
+          }}
+        >
+          <CardHeader>
+            <Heading size="sm">
+              {leg.start.name +
+                "에서 " +
+                (leg.sectionTime / 60).toFixed() +
+                "분"}
+            </Heading>
+          </CardHeader>
+          <CardBody>{verbose}</CardBody>
+        </Card>
+      </Flex>
+    </>
+  );
+}
+
+function Progressbar(
+  legs: any[] // itineraries
+) {
+  var progressList: any[] = [];
+  for (var i = 0; i < legs.length; i++) {
+    if (legs[i].sectionTime > 0) progressList.push(verboseBox(legs[i]));
+  }
+
+  return (
+    <>
+      <Stack direction="column" height="-webkit-fit-content">
+        {progressList}
+      </Stack>
+    </>
+  );
+}
 
 function mainInfo() {
-  var progressbar = Progressbar(data.itineraries);
   return (
     <>
       <Stack
@@ -19,78 +107,105 @@ function mainInfo() {
           },
         }}
       >
-        {progressbar}
-        {progressbar}
+        {data.routes.map((way, i) => (
+          <Box key={i} flexDirection="column">
+            {Progressbar(way.legs)}
+            <Card
+              width="100%"
+              height="100px"
+              display="flex"
+              shadow="lg"
+              marginTop="4"
+              direction="row"
+              alignItems="center"
+              p="4"
+              onClick={() => {
+                <Script id="onClick">{`
+									moveCenter(${way.legs[way.legs.length - 1].end.lat}, ${
+                  way.legs[way.legs.length - 1].end.lon
+                })
+								`}</Script>;
+              }}
+            >
+              <Text fontSize="2xl" m="4">
+                🚩
+              </Text>
+              <Text fontSize="md" as="b">
+                {way.legs[way.legs.length - 1].end.name}
+              </Text>
+            </Card>
+          </Box>
+        ))}
       </Stack>
     </>
   );
 }
 
+function placeMarker(leg: any) {
+  return (
+    <Script
+      id={leg.start.name}
+    >{`makeMarker(${leg.start.lat}, ${leg.start.lon});`}</Script>
+  );
+}
+
 export default function Verbose() {
-  var data1: any;
-
-  const fetch = require("node-fetch");
-
-  const url = "https://apis.openapi.sk.com/transit/routes";
-  const options = {
-    method: "POST",
-    headers: {
-      accept: "application/json",
-      "content-type": "application/json",
-      appKey: "UN27oUnc8Ma0gwH3grXRaat6I163mp0W1Osa5VUy",
-    },
-    body: JSON.stringify({
-      startX: "126.926493082645",
-      startY: "37.6134436427887",
-      endX: "127.126936754911",
-      endY: "37.5004198786564",
-      lang: 0,
-      format: "json",
-      count: 10,
-      searchDttm: "202301011200",
-    }),
-  };
-
-  fetch(url, options)
-    .then((res: any) => res.json())
-    .then((json: any) => (data1 = json))
-    .catch((err: any) => console.error("error:" + err));
-
-  console.log(data);
-
+  var scriptList: any[] = [];
+  var i = 0;
+  data.routes[0].legs.map((way, _) => {
+    scriptList.push(
+      <Script key={(i++).toString()} id={(i++).toString()}>{`
+		makeMarker(${way.start.lat}, ${way.start.lon});
+	`}</Script>
+    );
+  });
   return (
     <>
-      <Script
-        id="mapinit"
-        onLoad={() => {
-          console.log("map load");
-        }}
-      >
-        {`
-				function initTmap(x, y) {
-          var map = new Tmapv2.Map("map_div", {
-            center: new Tmapv2.LatLng(x, y),
-						width: "73%",
-            height: "99vh",
-            zoom: 17,
-          });
-          return map;
-        }
-      	initTmap(37.5652045, 126.98702028);`}
-      </Script>
       <Flex id="bg" width="100%" height="100%">
         <Flex height="95vh" m="4" flex={1} flexDirection="column">
           <HStack paddingBottom={2} height="max-content">
             <Text fontSize="xl" as="b" noOfLines={1} maxW="10vw">
-              {data.itineraries[0].legs[0].start.name}
+              {data.routes[0].legs[0].start.name}
             </Text>
             <Text fontSize="xl" as="b" noOfLines={1} maxW="15vw">
-              ➡️ {data.itineraries[0].legs[0].end.name}
+              ➡️ {data.routes[data.routes.length - 1].legs[0].end.name}
             </Text>
           </HStack>
 
           {mainInfo()}
         </Flex>
+        <Script id="mapinit">
+          {`
+				var map = new Tmapv2.Map("map_div", {
+					center: new Tmapv2.LatLng(${data.routes[0].legs[0].start.lat}, ${data.routes[0].legs[0].start.lon}),
+					width: "73%",
+					height: "99vh",
+					zoom: 17,
+				});
+			
+				function makeMarker(x, y) {
+					var marker = new Tmapv2.Marker({
+						position: new Tmapv2.LatLng(x, y),
+						map: map,
+					});
+				}
+
+				function makeLine(path, color) {
+					var polyline = new Tmapv2.Polyline({
+						path: path,
+					 	strokeColor: "#" + color,
+					 	strokeWeight: 6,
+					 	map: map
+				 	});
+				}
+
+				function moveCenter(x, y) {
+					console.log("hihi");
+					map.setCenter(new Tmapv2.LatLng(x, y));
+				}
+				`}
+        </Script>
+        {scriptList}
         <div id="map_div"></div>
       </Flex>
     </>
